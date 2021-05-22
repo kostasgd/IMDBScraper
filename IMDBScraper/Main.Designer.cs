f015471c5835c2caea4288d4102598ce21c1b294
kostas georgiadis <konstantinos.georgiadis@windowslive.com>
2021-05-22 13:14:25 +0300

    ll

---
+++ IMDBScraper/Main.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace IMDBScraper
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.materialTabSelector1 = new MaterialSkin.Controls.MaterialTabSelector();
+            this.lbNews = new System.Windows.Forms.ListBox();
+            this.materialLabel1 = new MaterialSkin.Controls.MaterialLabel();
+            this.materialLabel2 = new MaterialSkin.Controls.MaterialLabel();
+            this.lbstream = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // materialTabSelector1
+            // 
+            this.materialTabSelector1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.materialTabSelector1.BaseTabControl = null;
+            this.materialTabSelector1.Depth = 0;
+            this.materialTabSelector1.Location = new System.Drawing.Point(1, 71);
+            this.materialTabSelector1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialTabSelector1.Name = "materialTabSelector1";
+            this.materialTabSelector1.Size = new System.Drawing.Size(861, 11);
+            this.materialTabSelector1.TabIndex = 0;
+            this.materialTabSelector1.Text = "materialTabSelector1";
+            // 
+            // lbNews
+            // 
+            this.lbNews.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbNews.BackColor = System.Drawing.Color.Yellow;
+            this.lbNews.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbNews.FormattingEnabled = true;
+            this.lbNews.ItemHeight = 17;
+            this.lbNews.Location = new System.Drawing.Point(1, 110);
+            this.lbNews.Name = "lbNews";
+            this.lbNews.Size = new System.Drawing.Size(861, 72);
+            this.lbNews.TabIndex = 1;
+            // 
+            // materialLabel1
+            // 
+            this.materialLabel1.AutoSize = true;
+            this.materialLabel1.Depth = 0;
+            this.materialLabel1.Font = new System.Drawing.Font("Roboto", 11F);
+            this.materialLabel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialLabel1.Location = new System.Drawing.Point(12, 85);
+            this.materialLabel1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel1.Name = "materialLabel1";
+            this.materialLabel1.Size = new System.Drawing.Size(47, 19);
+            this.materialLabel1.TabIndex = 2;
+            this.materialLabel1.Text = "News";
+            // 
+            // materialLabel2
+            // 
+            this.materialLabel2.AutoSize = true;
+            this.materialLabel2.Depth = 0;
+            this.materialLabel2.Font = new System.Drawing.Font("Roboto", 11F);
+            this.materialLabel2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialLabel2.Location = new System.Drawing.Point(12, 194);
+            this.materialLabel2.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel2.Name = "materialLabel2";
+            this.materialLabel2.Size = new System.Drawing.Size(81, 19);
+            this.materialLabel2.TabIndex = 3;
+            this.materialLabel2.Text = "Streaming ";
+            // 
+            // lbstream
+            // 
+            this.lbstream.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lbstream.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbstream.FormattingEnabled = true;
+            this.lbstream.ItemHeight = 17;
+            this.lbstream.Location = new System.Drawing.Point(1, 225);
+            this.lbstream.Name = "lbstream";
+            this.lbstream.Size = new System.Drawing.Size(861, 89);
+            this.lbstream.TabIndex = 4;
+            this.lbstream.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.lbstream_MouseDoubleClick);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(859, 385);
+            this.Controls.Add(this.lbstream);
+            this.Controls.Add(this.materialLabel2);
+            this.Controls.Add(this.materialLabel1);
+            this.Controls.Add(this.lbNews);
+            this.Controls.Add(this.materialTabSelector1);
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Imdb Scraper";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MaterialSkin.Controls.MaterialTabSelector materialTabSelector1;
+        private System.Windows.Forms.ListBox lbNews;
+        private MaterialSkin.Controls.MaterialLabel materialLabel1;
+        private MaterialSkin.Controls.MaterialLabel materialLabel2;
+        private System.Windows.Forms.ListBox lbstream;
+    }
+}
---
+++ IMDBScraper/Main.cs
@@ -0,0 +1,102 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IMDBScraper
+{
+    public partial class Main : MaterialSkin.Controls.MaterialForm
+    {
+        public Main()
+        {
+            InitializeComponent();
+            var skinmanager = MaterialSkin.MaterialSkinManager.Instance;
+            skinmanager.AddFormToManage(this);
+            skinmanager.Theme = MaterialSkin.MaterialSkinManager.Themes.DARK;
+            skinmanager.ColorScheme = new MaterialSkin.ColorScheme(MaterialSkin.Primary.Yellow400, MaterialSkin.Primary.BlueGrey900,
+                MaterialSkin.Primary.BlueGrey500, MaterialSkin.Accent.LightBlue200, MaterialSkin.TextShade.BLACK);
+            HtmlAgilityPack.HtmlWeb web = new HtmlAgilityPack.HtmlWeb();
+            HtmlAgilityPack.HtmlDocument doc = web.Load("https://www.imdb.com/") ;
+            var title = doc.DocumentNode.SelectNodes("//h3[contains(@class,'title')]");
+            foreach (var item in title)
+            {
+               // Console.WriteLine(item.InnerText.ToString());
+            }
+
+
+            //κομβος που παιρνει τις  μερες
+            //var nbt = doc.DocumentNode.SelectNodes("//div[contains(@class,'born-today-name')]");
+            //var nba = doc.DocumentNode.SelectNodes("//div[contains(@class,'born-today-age')]");
+
+            //foreach (var item in nbt)
+            //{
+            //    Console.WriteLine(item.InnerText);
+            //}
+            //foreach (var item in nba)
+            //{
+            //    Console.WriteLine(item.InnerText);
+            //}
+            //getNews();
+            var chromeOptions = new ChromeOptions();//δημιουργια αντικειμενου για να περασουμε τις επιλογες που θελουμε για το chrome driver
+            chromeOptions.AddArguments("headless");//επιλογη ωστε το chrome driver να δουλευει χωρις κεφαλη
+            var experimentalFlags = new List<string>();
+            string mediasrc = "";//μεταβλητη που θα την γεμισουμε μετα
+            var driverService = ChromeDriverService.CreateDefaultService();
+            driverService.HideCommandPromptWindow = true;//κρυψιμο του prompt του selenium
+            ChromeDriver driver = new ChromeDriver(driverService, chromeOptions);//δημιουργια chrome driver αντικειμενου και περασμα παραμετρων
+            driver.Navigate().GoToUrl("https://www.imdb.com/");
+            chromeOptions.AddLocalStatePreference("browser.enabled_labs_experiments",
+                experimentalFlags);
+            var cookies = driver.Manage().Cookies.AllCookies;
+            List<IWebElement> pdate = driver.FindElements(By.XPath("//span[contains(@class,'ipc-rating-star ipc-rating-star--baseAlt ipc-rating-star--imdb')]")).ToList();
+            foreach(var k in pdate)
+            {
+                Console.WriteLine(k.Text);
+            }
+            String columns = "{0, -55}{1, -35}{2, -35}";
+            lbstream.Items.Add(String.Format(columns, "Title", "Rating", "Link"));
+            lbstream.Items.Add(String.Format(columns, "LOTR", "9", "www.google.com"));
+        }
+        public void getNews()
+        {
+            var chromeOptions = new ChromeOptions();//δημιουργια αντικειμενου για να περασουμε τις επιλογες που θελουμε για το chrome driver
+            chromeOptions.AddArguments("headless");//επιλογη ωστε το chrome driver να δουλευει χωρις κεφαλη
+            var experimentalFlags = new List<string>();
+            string mediasrc = "";//μεταβλητη που θα την γεμισουμε μετα
+            var driverService = ChromeDriverService.CreateDefaultService();
+            driverService.HideCommandPromptWindow = true;//κρυψιμο του prompt του selenium
+            ChromeDriver driver = new ChromeDriver(driverService, chromeOptions);//δημιουργια chrome driver αντικειμενου και περασμα παραμετρων
+            driver.Navigate().GoToUrl("https://www.imdb.com/");
+            chromeOptions.AddLocalStatePreference("browser.enabled_labs_experiments",
+                experimentalFlags);
+            var cookies = driver.Manage().Cookies.AllCookies;
+            List<IWebElement> headline = driver.FindElements(By.ClassName("headline")).ToList();//κομβος που παιρνει τις  μερες
+            List<IWebElement> pdate = driver.FindElements(By.ClassName("publicationDate")).ToList();
+            List<IWebElement> source = driver.FindElements(By.ClassName("source")).ToList();
+
+            for (int i=0;i< headline.Count; i++)
+            {
+                string s = headline[i].Text + "\n " +pdate[i].Text +" - " +source[i].Text;
+                lbNews.Items.Add(s);
+                s = "";
+            }
+        }
+
+        private void lbstream_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            var list = (ListBox)sender;
+
+            // This is your selected item
+            object item = list.SelectedItem;
+            var link = item.ToString().Split("\t\n ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries).Where(s => s.StartsWith("http://") || s.StartsWith("www.") || s.StartsWith("https://"));
+            System.Diagnostics.Process.Start("http://google.com");
+        }
+    }
+}
